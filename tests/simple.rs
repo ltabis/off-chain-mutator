437ea7dee5878076cbd944854554b9077e525715
ltabis <ltabis.pro@gmail.com>
2022-08-20 18:15:22 +0200

    test: add test for all available operations.

--- tests/simple.rs
+++ tests/simple.rs
@@ -2,29 +2,16 @@
 mod test {
     const DATA_PATH: &str = "./tests/data/";
 
-    use off_chain_mutator::{
-        accounts::Account,
-        transaction::{History, Transaction},
-    };
+    use off_chain_mutator::{accounts::Account, transaction::History};
 
     #[test]
     fn test_simple_deposits() {
-        let transactions = History::new(
-            csv::ReaderBuilder::new()
-                .from_path(std::path::PathBuf::from_iter([
-                    DATA_PATH,
-                    "simple-deposit.csv",
-                ]))
-                .expect("could not open csv database")
-                .deserialize()
-                .map(|result| result.expect("could not deserialize csv"))
-                .collect::<Vec<Transaction>>(),
-        );
+        let transactions = History::from_path(&format!("{DATA_PATH}/simple-deposit.csv")).unwrap();
 
         let mut clients = transactions
             .0
             .iter()
-            .map(|transaction| (transaction.client_id, Account::new(transaction.client_id)))
+            .map(|transaction| (transaction.client, Account::new(transaction.client)))
             .collect::<std::collections::HashMap<_, _>>();
 
         transactions.update_accounts(&mut clients);
@@ -44,22 +31,68 @@ mod test {
                     available: 1.0,
                     total: 1.0,
                     held: 0.0,
-                    locked: false
+                    locked: false,
+                    disputed: None,
                 },
                 Account {
                     client_id: 2,
                     available: 11.0,
                     total: 11.0,
                     held: 0.0,
-                    locked: false
+                    locked: false,
+                    disputed: None,
                 },
                 Account {
                     client_id: 10,
                     available: 2.0,
                     total: 2.0,
                     held: 0.0,
-                    locked: false
+                    locked: false,
+                    disputed: None,
                 }
+            ]
+        );
+    }
+
+    #[test]
+    fn test_simple_operations() {
+        let transactions =
+            History::from_path(&format!("{DATA_PATH}/simple-operations.csv")).unwrap();
+
+        let mut clients = transactions
+            .0
+            .iter()
+            .map(|transaction| (transaction.client, Account::new(transaction.client)))
+            .collect::<std::collections::HashMap<_, _>>();
+
+        transactions.update_accounts(&mut clients);
+
+        let mut accounts = clients
+            .into_iter()
+            .map(|(_, account)| account)
+            .collect::<Vec<_>>();
+
+        accounts.sort_unstable_by_key(|account| account.client_id);
+
+        assert_eq!(
+            accounts,
+            vec![
+                Account {
+                    client_id: 1,
+                    available: 39.90,
+                    total: 39.90,
+                    held: 0.0,
+                    locked: false,
+                    disputed: None,
+                },
+                Account {
+                    client_id: 2,
+                    available: 0.0,
+                    total: 0.0,
+                    held: 0.0,
+                    locked: true,
+                    disputed: None,
+                },
             ]
         );
     }
